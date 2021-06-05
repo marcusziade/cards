ffde4dea7ad5300636c3d8ac7309abbe898fe086
marcusziade <guitaripod@icloud.com>
2021-06-05 15:40:29 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	cards := deck{
+		newCard(),
+		newCard(),
+	}
+
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+}
+
+func newCard() string {
+	return "Ace of Spades"
+}
