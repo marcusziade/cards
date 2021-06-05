0db5091edf99aa5602a35e5e2fa08c9df808dc74
marcusziade <guitaripod@icloud.com>
2021-06-05 17:10:24 +0300

    Add shuffle method

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
 	// cards := newDeck()
 	// cards.saveToFile("my_cards")
+	// cards.print()
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
